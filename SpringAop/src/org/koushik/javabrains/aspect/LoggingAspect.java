22b8b2f52d40cf570b8822237368e6b2189f47d5
Adrian <adrian8535@wp.pl>
2013-02-15 15:37:09 +0100

    tut28

--- SpringAop/src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ SpringAop/src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -2,13 +2,18 @@ package org.koushik.javabrains.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-	@Before("execution(public String getName())")
+	@Before("allGetters()")
 	public void LoggingAdvice() {
-		
-		System.out.println("Advice run. Get method call");
-		
+		System.out.println("Advice run. Get method call");	
 	}
+	@Before("allGetters()")
+	public void SecondAdvice() {
+		System.out.println("2nd executed");
+	}
+	@Pointcut("execution(* get*())")
+	public void allGetters() {}
 }
