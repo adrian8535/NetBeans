2ec9985d00fe05756c9023de97fb2b0e372d15b2
Adrian <adrian8535@wp.pl>
2013-02-18 11:12:51 +0100

    35

--- SpringAop/src/org/koushik/javabrains/AopMain.java
+++ SpringAop/src/org/koushik/javabrains/AopMain.java
@@ -1,6 +1,8 @@
 package org.koushik.javabrains;
 
+import org.koushik.javabrains.service.FactoryService;
 import org.koushik.javabrains.service.ShapeService;
+import org.koushik.javabrains.service.FactoryService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,11 +12,13 @@ public class AopMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		//ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
 		
-		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+		FactoryService factoryService = new FactoryService();
+		ShapeService shapeService = (ShapeService) factoryService.getBean("shapeService");
+		
 		shapeService.getCircle();
-		//System.out.println(shapeService.getCircle().getName());
 		
 	}
 
--- SpringAop/src/org/koushik/javabrains/aspect/LoggingAspect.java
+++ SpringAop/src/org/koushik/javabrains/aspect/LoggingAspect.java
@@ -10,28 +10,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-@Aspect
 public class LoggingAspect {
-	@Before("allCircleMethods()")
-	public void LoggingAdvice(JoinPoint joinPoint) {
-		//System.out.println(joinPoint.toString());	
-	}
-/*	@Before("allGetters()")
-	public void SecondAdvice() {
-		System.out.println("2nd executed");
-	}*/
 	
-	@AfterReturning(pointcut="args(name)", returning="returnString")
-	public void StringArgumentMethods(String name, Object returnString) {
-		System.out.println("Method that takes string args has been called. Value: " + name + ". Output: " + returnString);
-	}
-	
-	@AfterThrowing(pointcut="args(name)", throwing="ex")
-	public void ExceptionAdvice(String name, Exception ex) {
-		System.out.println("Exception thrown " + ex);
-	}
-	
-	@Around("allGetters()")
 	public Object myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) { 
 		
 		Object returnValue = null; 
@@ -47,11 +27,7 @@ public class LoggingAspect {
 		return returnValue;
 	}
 	
-	
-	@Pointcut("execution(* get*())")
-	public void allGetters() {}
-	
-	@Pointcut("within(org.koushik.javabrains.model.Circle)")
-	public void allCircleMethods() {}
-	
+	public void loggingAdvice() {
+		System.out.println("Logging from advice");
+	}
 }
