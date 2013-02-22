969abc41df74ab41fc5ce150aba978a0358e44fc
Adrian <adrian8535@wp.pl>
2013-02-22 12:39:56 +0100

    5

--- SimpleServletProject/src/org/koushik/javabrains/XmlServlet.java
+++ SimpleServletProject/src/org/koushik/javabrains/XmlServlet.java
@@ -1,6 +1,7 @@
 package org.koushik.javabrains;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class XmlServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("XML Servlet called");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello from GET" + userName);
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello from POST " + userName);
+		
 	}
 }
