4b566f4e7018c7958d2056635df147bac697819b
Adrian <adrian8535@wp.pl>
2013-02-23 10:02:39 +0100

    16

---
+++ LoginApp/src/org/koushik/javabrains/service/LoginService.java
@@ -0,0 +1,11 @@
+package org.koushik.javabrains.service;
+
+public class LoginService {
+
+	public boolean authenticate(String userId, String password) {
+		if (password == null || password.trim() == "") {
+			return false;
+		}
+		return true;
+	}
+}
