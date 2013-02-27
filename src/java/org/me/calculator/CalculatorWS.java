41b9b268e233df62f60ac6d9e9c71e6041746300
1 <1@192.168.0.9>
2013-02-27 18:27:37 +0100

    calc1

---
+++ src/java/org/me/calculator/CalculatorWS.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.calculator;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author 1
+ */
+@WebService(serviceName = "CalculatorWS")
+@Stateless()
+public class CalculatorWS {
+
+
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        int k = i + j;
+        return k;
+    }
+}
